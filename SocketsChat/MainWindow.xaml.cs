36da3669d3029495abaf162f0fe1bddf98f5901a
Aleksander <scorpinoc@bigmir.net>
2017-07-24 16:57:33 +0300

    Добавьте файлы проекта.

---
+++ SocketsChat/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SocketsChat
+{
+    /// <summary>
+    ///     Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
