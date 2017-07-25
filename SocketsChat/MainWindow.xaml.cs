360643d12c8f4996802136bb44686021b077280f
Aleksander <scorpinoc@bigmir.net>
2017-07-25 21:42:15 +0300

    Messages converted to objects and sends as JSON

--- SocketsChat/MainWindow.xaml.cs
+++ SocketsChat/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Input;
+using Newtonsoft.Json;
 
 namespace SocketsChat
 {
@@ -44,7 +45,7 @@ namespace SocketsChat
         #region Methods
 
         #region Private
-        
+
         private void InvokeInMainThread(Action callback) => Dispatcher.Invoke(callback);
 
         private void OpenServer(EndPoint endPoint)
@@ -74,21 +75,50 @@ namespace SocketsChat
             {
                 int count;
                 var bytes = new byte[4096];
-
-                var appendMessage =
-                    new Action<byte[]>(
-                        message =>
-                            InvokeInMainThread(
-                                () => ChatBox.AppendText("other: " + Encoding.UTF8.GetString(message) + "\n")));
+                var messageText = string.Empty;
 
                 while ((count = accept.Receive(bytes)) > 0)
                     if (count == 4096)
-                        appendMessage(bytes);
+                        messageText += Encoding.UTF8.GetString(bytes);
                     else
                     {
-                        appendMessage(bytes.Take(count).ToArray());
+                        messageText += Encoding.UTF8.GetString(bytes.Take(count).ToArray());
                         break;
                     }
+
+                // todo ! refactor
+                switch (JsonConvert.DeserializeObject<JsonType>(messageText).Type)
+                {
+                    case nameof(Message):
+                    {
+                        var message = JsonConvert.DeserializeObject<Message>(messageText);
+                        message.RecieveTime = DateTime.Now;
+                        AddMessage(message);
+                        // todo ! delete and replace
+                        using (var socket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP))
+                        {
+                            var ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), int.Parse("3000"));
+                            socket1.Connect(ipEndPoint);
+
+                            var answer = new Answer(message.Number, DateTime.Now);
+
+                            socket1.Send(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(answer)));
+                        }
+                        break;
+                    }
+                    case nameof(Answer):
+                    {
+                        var answer = JsonConvert.DeserializeObject<Answer>(messageText);
+                        var message = new Message(answer.Number, "server", "message recieved by client")
+                                      {
+                                          RecieveTime = answer.AnswerTime
+                                      };
+                        AddMessage(message);
+                        break;
+                    }
+                    default:
+                        throw new TypeAccessException("Type not implemented");
+                }
             }
         }
 
@@ -101,10 +131,13 @@ namespace SocketsChat
                     var ipEndPoint = new IPEndPoint(IPAddress.Parse(ConnectIp.Text), int.Parse(ConnectPort.Text));
                     socket.Connect(ipEndPoint);
 
-                    var message = string.Empty;
-                    InvokeInMainThread(() => message = Message.Text);
-                    InvokeInMainThread(() => ChatBox.AppendText("I: " + message + "\n"));
-                    socket.Send(Encoding.UTF8.GetBytes(message));
+                    var messageText = string.Empty;
+                    InvokeInMainThread(() => messageText = MessageText.Text);
+                    var message = new Message(0, "me", messageText);
+
+                    AddMessage(message);
+
+                    socket.Send(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message)));
                 }
             }
             catch (Exception e)
@@ -113,7 +146,54 @@ namespace SocketsChat
             }
         }
 
+        private void AddMessage(Message message)
+        {
+            var date = "(" + (message.RecieveTime?.ToLongTimeString() ?? "sending...") + ")";
+            InvokeInMainThread(() => ChatBox.AppendText($"{date} {message.NickName}: {message.MessageText}\n"));
+        }
+
         #endregion
+
+        #endregion
+
+        #region Inner Classes
+
+        private class JsonType
+        {
+            public string Type { get; }
+
+            public JsonType(string type = null)
+            {
+                Type = string.IsNullOrEmpty(type) ? GetType().Name : type;
+            }
+        }
+
+        private class Message : JsonType
+        {
+            public uint Number { get; }
+            public string NickName { get; }
+            public DateTime? RecieveTime { get; set; }
+            public string MessageText { get; }
+
+            public Message(uint number, string nickName, string messageText)
+            {
+                Number = number;
+                NickName = nickName;
+                MessageText = messageText;
+            }
+        }
+
+        private class Answer : JsonType
+        {
+            public uint Number { get; }
+            public DateTime AnswerTime { get; }
+
+            public Answer(uint number, DateTime answerTime)
+            {
+                Number = number;
+                AnswerTime = answerTime;
+            }
+        }
 
         #endregion
     }
