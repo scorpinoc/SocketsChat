f9f998f9bd8741460165a3487202b2fe8a197aa9
Aleksander <scorpinoc@bigmir.net>
2017-08-11 17:31:54 +0300

    TryCommand minor rework and adding of xml-documentation.

--- ChatServer/Utility/Commands/TryCommand.cs
+++ ChatServer/Utility/Commands/TryCommand.cs
@@ -4,15 +4,89 @@ using System.Windows.Input;
 
 namespace ChatServer.Utility.Commands
 {
+    /// <summary>
+    ///     Provides factory methods for wrapping <see cref="ICommand" /> methods in ry-catch-finally blocks.
+    /// </summary>
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public static class TryCommand
     {
+        #region Static
+
+        // todo catch null value to ignore exception
+        /// <summary>
+        ///     Creates <see cref="ICommand" /> whrere <paramref name="command" /> <seealso cref="ICommand.Execute" /> and
+        ///     <seealso cref="ICommand.CanExecute" /> methods wrapped in try-catch-finally block.
+        /// </summary>
+        /// <param name="command"><see cref="ICommand" /> to wrap in try-catch-finally.</param>
+        /// <param name="executeCatch">Action for catch block of <seealso cref="ICommand.Execute" /> method.</param>
+        /// <param name="canExecuteCatch">
+        ///     Function for catch block of <seealso cref="ICommand.CanExecute" /> method. Returns
+        ///     <see cref="bool" />.
+        /// </param>
+        /// <param name="executeFinally">
+        ///     Action for finally block of <seealso cref="ICommand.Execute" /> method. Argument types
+        ///     must be the same as in original command.
+        /// </param>
+        /// <param name="canExecuteFinally">
+        ///     >Action for finally block of <seealso cref="ICommand.CanExecute" /> method. Argument
+        ///     types must be the same as in original command.
+        /// </param>
         public static ICommand CreateCommand<T1, T2>(ICommand command,
-            Action<Exception> @executeCatch,
-            Func<Exception, bool> @canExecuteCatch,
-            Action<T1, T2> @finally = null) => new MultiParametersCommand(command, @executeCatch, @canExecuteCatch, @finally);
+            Action<Exception> executeCatch,
+            Func<Exception, bool> canExecuteCatch,
+            Action<T1, T2> executeFinally = null,
+            Action<T1, T2> canExecuteFinally = null)
+            =>
+                new MultiParametersCommand(command, new ExecuteCatchWrapper(executeCatch, executeFinally),
+                    new CanExecuteCatchWrapper(canExecuteCatch, canExecuteFinally));
+
+        #endregion
 
         #region Nested Types
+
+        private class FinallyWrapper
+        {
+            #region Properties
+
+            internal Delegate Finally { get; }
+
+            #endregion
+
+            protected FinallyWrapper(Delegate @finally)
+            {
+                Finally = @finally;
+            }
+        }
+
+        private class ExecuteCatchWrapper : FinallyWrapper
+        {
+            #region Properties
+
+            internal Action<Exception> Catch { get; }
+
+            #endregion
+
+            public ExecuteCatchWrapper(Action<Exception> @catch, Delegate @finally = null) : base(@finally)
+            {
+                if (@catch == null) throw new ArgumentNullException(nameof(@catch));
+                Catch = @catch;
+            }
+        }
+
+        private class CanExecuteCatchWrapper : FinallyWrapper
+        {
+            #region Properties
+
+            internal Func<Exception, bool> Catch { get; }
+
+            #endregion
+
+            public CanExecuteCatchWrapper(Func<Exception, bool> @catch, Delegate @finally = null) : base(@finally)
+            {
+                if (@catch == null) throw new ArgumentNullException(nameof(@catch));
+                Catch = @catch;
+            }
+        }
 
         private abstract class TryCommandBase : ICommand
         {
@@ -21,28 +95,24 @@ namespace ChatServer.Utility.Commands
             public event EventHandler CanExecuteChanged;
 
             private ICommand Command { get; }
-            private Action<Exception> ExecuteCatch { get; }
-            private Func<Exception, bool> CanExecuteCatch { get; }
-            protected Delegate Finally { get; }
+
+            private ExecuteCatchWrapper ExecuteCatchWrapper { get; }
+            private CanExecuteCatchWrapper CanExecuteCatchWrapper { get; }
 
             #endregion
 
             protected TryCommandBase(ICommand command,
-                Action<Exception> @executeCatch,
-                Func<Exception, bool> @canExecuteCatch,
-                Delegate @finally = null
-                )
+                ExecuteCatchWrapper executeCatchWrapper,
+                CanExecuteCatchWrapper canExecuteCatchWrapper)
             {
                 if (command == null) throw new ArgumentNullException(nameof(command));
-                if (executeCatch == null) throw new ArgumentNullException(nameof(executeCatch));
-                if (canExecuteCatch == null) throw new ArgumentNullException(nameof(canExecuteCatch));
+                if (executeCatchWrapper == null) throw new ArgumentNullException(nameof(executeCatchWrapper));
+                if (canExecuteCatchWrapper == null) throw new ArgumentNullException(nameof(canExecuteCatchWrapper));
 
                 Command = command;
-                ExecuteCatch = @executeCatch;
-                CanExecuteCatch = @canExecuteCatch;
-                Finally = @finally;
-
-                command.CanExecuteChanged += (sender, args) => CanExecuteChanged?.Invoke(this, args);
+                ExecuteCatchWrapper = executeCatchWrapper;
+                CanExecuteCatchWrapper = canExecuteCatchWrapper;
+                Command.CanExecuteChanged += (sender, args) => CanExecuteChanged?.Invoke(this, args);
             }
 
             #region Methods
@@ -55,12 +125,11 @@ namespace ChatServer.Utility.Commands
                 }
                 catch (Exception e)
                 {
-                    return CanExecuteCatch(e);
+                    return CanExecuteCatchWrapper.Catch(e);
                 }
                 finally
                 {
-                    if (Finally != null)
-                        FinallyImplementation(parameter);
+                    Finally(CanExecuteCatchWrapper, parameter);
                 }
             }
 
@@ -73,16 +142,21 @@ namespace ChatServer.Utility.Commands
                 }
                 catch (Exception e)
                 {
-                    ExecuteCatch(e);
+                    ExecuteCatchWrapper.Catch(e);
                 }
                 finally
                 {
-                    if (Finally != null)
-                        FinallyImplementation(parameter);
+                    Finally(ExecuteCatchWrapper, parameter);
                 }
             }
 
-            protected abstract void FinallyImplementation(object parameter);
+            private void Finally(FinallyWrapper finallyWrapper, object parameter)
+            {
+                if (finallyWrapper.Finally != null)
+                    FinallyImplementation(finallyWrapper, parameter);
+            }
+
+            protected abstract void FinallyImplementation(FinallyWrapper finallyWrapper, object parameter);
 
             #endregion
         }
@@ -90,14 +164,18 @@ namespace ChatServer.Utility.Commands
         private class MultiParametersCommand : TryCommandBase
         {
             public MultiParametersCommand(ICommand command,
-                Action<Exception> executeCatch,
-                Func<Exception, bool> canExecuteCatch,
-                Delegate @finally = null) : base(command, executeCatch, canExecuteCatch, @finally)
+                ExecuteCatchWrapper executeCatchWrapper,
+                CanExecuteCatchWrapper canExecuteCatchWrapper)
+                : base(command, executeCatchWrapper, canExecuteCatchWrapper)
             {
             }
 
-            protected override void FinallyImplementation(object parameter)
-                => Finally.DynamicInvoke((object[]) parameter);
+            #region Methods
+
+            protected override void FinallyImplementation(FinallyWrapper finallyWrapper, object parameter)
+                => finallyWrapper.Finally.DynamicInvoke((object[]) parameter);
+
+            #endregion
         }
 
         #endregion
